643d03857dd19babfbfe50201c5b73cb7645cde5
zhangwei <zhangwei19890518@gmail.com>
2019-03-18 13:08:57 +0800

    complete initial version

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess my secket key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = ('zhang', 'zhangwei19890518@gmail.com')
+    ADMINS = ['zhangwei19890518@gmail.com']
+
+    POSTS_PER_PAGE = 3
+
+    LANGUAGES = ['en', 'zh']
